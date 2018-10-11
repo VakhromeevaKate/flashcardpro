d02210d98d680162b67d4633b674dca42566a2db
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2018-10-11 16:18:35 +0300

    Data added

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -1,13 +1,32 @@
 import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-export default class Stack extends PureComponent {
+class Stack extends PureComponent {
     render() {
+        const { title, cards } = this.props.stack;
         return (
             <div>
             <Link to='/'>Home</Link>
-                <h3>example</h3>
+                <h3>{title}</h3>
+                <br />
+                {
+                    cards.map( (card) => {
+                        return (
+                            <div key={card.id}>
+                                <div>{card.prompt}</div>
+                                <div>{card.answer}</div>
+                            </div>
+                        );
+                    })
+                }
             </div>
         );
     }
 }
+
+function mapStateToProps(state) {
+    return { stack: state.stack};
+}
+
+export default connect(mapStateToProps, null)(Stack);
--- src/components/StackList.js
+++ src/components/StackList.js
@@ -1,15 +1,21 @@
 import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import stacks from '../data/stacks.json';
+import { setStack } from '../actions';
 
-export default class StackList extends PureComponent {
+class StackList extends PureComponent {
     render() {
         return (
             <div>
                 {
                     stacks.map(stack => {
                         return (
-                            <Link to='/stack' key={stack.id}>
+                            <Link 
+                                to='/stack' 
+                                key={stack.id}
+                                onClick={() => this.props.setStack(stack)}
+                            >
                                 <h4>{stack.title}</h4>
                             </Link>
                         );
@@ -19,4 +25,6 @@ export default class StackList extends PureComponent {
         );
     }
 }
+
+export default connect(null, { setStack })(StackList);
 
